00fe151bae133ca74b2b0b1b62d980258b528bfa
Ilana Mostovski <ilana.mostovski@gmail.com>
2018-03-20 17:33:06 +0200

    init + render images

--- js/main.js
+++ js/main.js
@@ -1,10 +1,23 @@
 /* */
 'use strict'
-var gImgs = [
-             {id: 1, url: 'img/img01.jpg', keywords: ['tree','green','mountain','field']},
-             {id: 2, url: 'img/img02.jpg', keywords: ['water','field','sunrise','sky']},
-             {id: 3, url: 'img/img03.jpg', keywords: ['wheat','field','sky']},
-            ]; 
+
+var gNextId;
+var gImgs;
+
+// var gImgs = [
+//              {id: 1, url: 'img/img01.jpg', keywords: ['tree','green','mountain','field']},
+//              {id: 2, url: 'img/img02.jpg', keywords: ['water','field','sunrise','sky']},
+//              {id: 3, url: 'img/img03.jpg', keywords: ['wheat','field','sky']},
+
+//              {id: 4, url: 'img/img01.jpg', keywords: ['tree','green','mountain','field']},
+//              {id: 5, url: 'img/img02.jpg', keywords: ['water','field','sunrise','sky']},
+//              {id: 6, url: 'img/img03.jpg', keywords: ['wheat','field','sky']},
+//              {id: 7, url: 'img/img01.jpg', keywords: ['tree','green','mountain','field']},
+
+//              {id: 8, url: 'img/img02.jpg', keywords: ['water','field','sunrise','sky']},
+//              {id: 9, url: 'img/img03.jpg', keywords: ['wheat','field','sky']},
+//              {id: 10, url: 'img/img01.jpg', keywords: ['tree','green','mountain','field']},
+//             ]; 
 var gMeme = {selectedImgId: 5,     
                   txts: [{
                             line: 'I never eat Falafel',
@@ -14,3 +27,48 @@ var gMeme = {selectedImgId: 5,
                         }
                         ]
             };
+
+function init(){
+    gNextId = 0;
+    gImgs = createImgs();
+    renderImgs(gImgs);
+}
+
+function createImgs(){
+    var imgs = [];
+
+    imgs.push(createImg('img/img01.jpg', ['tree','green','mountain','field']));
+    imgs.push(createImg('img/img02.jpg', ['water','field','sunrise','sky']));
+    imgs.push(createImg('img/img03.jpg', ['wheat','field','sky']));
+
+    //TODO: fix data
+    imgs.push(createImg('img/img01.jpg', ['tree','green','mountain','field']));
+    imgs.push(createImg('img/img02.jpg', ['water','field','sunrise','sky']));
+    imgs.push(createImg('img/img03.jpg', ['wheat','field','sky']));
+    imgs.push(createImg('img/img01.jpg', ['tree','green','mountain','field']));
+    imgs.push(createImg('img/img02.jpg', ['water','field','sunrise','sky']));
+    imgs.push(createImg('img/img03.jpg', ['wheat','field','sky']));
+    imgs.push(createImg('img/img03.jpg', ['wheat','field','sky']));
+
+    return imgs;
+}
+
+function createImg(url, keywords){
+    return{
+        id: ++gNextId,
+        url: url,
+        keyWords: keywords
+    };
+}
+
+function renderImgs(imgs){
+    var strHtml = '';
+
+    var strHtmls = gImgs.map(function(img, idx){
+        strHtml = `<img id="${img.id}" src="${img.url}"/>`;
+        return strHtml;
+    });
+
+    var imgGrid = document.querySelector('.img-grid');
+    imgGrid.innerHTML = strHtmls.join('');
+}

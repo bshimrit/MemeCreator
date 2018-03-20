0aaf5c0f1a859539a244695d44f11d5fa3578473
bshimrit <bshimrit@gmail.com>
2018-03-20 16:41:53 +0200

    Basic layout

--- js/main.js
+++ js/main.js
@@ -1,2 +1,16 @@
 /* */
 'use strict'
+var gImgs = [
+             {id: 1, url: 'img/img01.jpg', keywords: ['tree','green','mountain','field']},
+             {id: 2, url: 'img/img02.jpg', keywords: ['water','field','sunrise','sky']},
+             {id: 3, url: 'img/img03.jpg', keywords: ['wheat','field','sky']},
+            ]; 
+var gMeme = {selectedImgId: 5,     
+                  txts: [{
+                            line: 'I never eat Falafel',
+                            size: 20,
+                            align: 'left',
+                            color: 'red'
+                        }
+                        ]
+            };
